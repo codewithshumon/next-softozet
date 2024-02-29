76cd6e0d1dac4b030b7e061bb2238f16e2c5dc96
Shumon Khan <backlink.shumonkhan@gmail.com>
2024-02-29 18:04:52 +0600

    Working on service page

--- app/(pages)/services/page.tsx
+++ app/(pages)/services/page.tsx
@@ -51,11 +51,11 @@ const ServicesPage = () => {
             <div className=" ">
               <ServiceLottie
                 animation={mobileSecond}
-                className="!p-2 md:!p-5 !w-[300px] xs:!w-[400px] md:!w-[600px]"
+                className="!p-2 md:!p-5 !w-[310px] xs:!w-[430px] md:!w-[650px]"
               />
             </div>
           </div>
-          <div className="w-screen h-screen bg-[#2dbb89]"></div>
+          <div className="w-screen h-screen bg-[#c54f3b]"></div>
         </div>
 
         <div className="w-full h-full relative">
@@ -89,7 +89,7 @@ const ServicesPage = () => {
             <div className="">
               <ServiceLottie
                 animation={seoMan}
-                className="!p-2 md:!p-5 !w-[300px] xs:!w-[400px] md:!w-[600px]"
+                className="!p-2 md:!p-5 !w-[250px] xs:!w-[350px] md:!w-[550px]"
               />
             </div>
           </div>
--- app/components/services/ServiceText.tsx
+++ app/components/services/ServiceText.tsx
@@ -8,13 +8,19 @@ interface ServiceTextProps {
 
 const ServiceText: React.FC<ServiceTextProps> = ({ title, subTitle, text }) => {
   return (
-    <div className="w-[90%] mx-auto h-full flex flex-col items-center text-white">
+    <div className="w-[90%] mx-auto h-full flex flex-col items-center justify-center ">
       <div className="">
-        <h1>{title}</h1>
-        <h2>{subTitle}</h2>
+        <h1 className="text-[12] xs:text-[16px] md:text-[22px] xl:text-[28px] font-bold  text-[#FFFFFF]">
+          {title}
+        </h1>
+        <h2 className="text-[10] xs:text-[12px] md:text-[18px] xl:text-[22px] font-semibold text-[#eeeded]">
+          {subTitle}
+        </h2>
       </div>
-      <div>
-        <p>{text}</p>
+      <div className="">
+        <p className="text-[8] xs:text-[10px] md:text-[16px] xl:text-[18px] font-normal text-[#d4d4d4]">
+          {text}
+        </p>
       </div>
     </div>
   );
