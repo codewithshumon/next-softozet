e5cb1a6b4ef910cd550104372f9ccec4ff950e10
Shumon Khan <backlink.shumonkhan@gmail.com>
2024-06-30 15:32:23 +0600

    Fixed Header Issues

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useRef, useState } from "react";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { BiMenu } from "react-icons/bi";
 import gsap from "gsap";
 
@@ -25,6 +25,8 @@ const Header = () => {
   const scrollRef = useRef(scrollY);
   const [showSidebar, setShowSidebar] = useState(false);
   const [closeSidebar, setCloseSidebar] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+  const router = useRouter();
   const pathname = usePathname();
 
   //to avoid gsap target not found
@@ -39,6 +41,17 @@ const Header = () => {
   useEffect(() => {
     scrollRef.current = scrollY;
   }, [scrollY]);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    handleResize(); // Check initial size
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   useEffect(() => {
     gsap.to(".scroll-translate-up", {
@@ -92,8 +105,8 @@ const Header = () => {
                       <div className="">
                         <div className="text-[16px] xs:text-[28px] leading-7 font-[600] border-[3px] border-solid botton-gradient text-white">
                           <Button
-                            onClick={() => {}}
-                            title="FREE CONSULTATION"
+                            onClick={() => router.push("/contact")}
+                            title="GET IN TOUCH"
                             className=" px-2 py-[1px] cursor-pointer select-none"
                           />
                         </div>
@@ -104,7 +117,7 @@ const Header = () => {
                             key={index}
                             className={`${
                               menu.path === pathname
-                                ? "text-[#329aa1]"
+                                ? "text-[#e7d211]"
                                 : " text-[#ffffff]"
                             } text-[16px] xs:text-[24px] leading-7 xs:leading-10 font-[600]`}
                           >
@@ -120,64 +133,69 @@ const Header = () => {
           </div>
         </div>
       </div>
-      <header className=" fixed bg-transparent top-0 z-20 py-10 md:py-5 md:px-5 w-full h-[50px] md:h-[100px]">
-        <div className="container text-white flex items-center justify-between">
-          <nav className="flex items-center gap-3">
+      <header className=" fixed bg-transparent top-0 z-20 py-10 px-5 md:py-5 md:px-5 w-full h-[50px] md:h-[100px]">
+        <div className="w-full h-full header-bg-blur rounded-full px-0 py-0 md:px-5 md:py-8 flex items-center ">
+          <div className=" w-full text-white flex items-center justify-between overflow-hidden">
+            <nav className="flex items-center gap-3">
+              <div
+                className=""
+                style={{
+                  transform: `rotate(-${scrollY}deg)`,
+                  transition: "transform 0.2s",
+                }}
+              >
+                <SvgLogo
+                  width={isMobile ? 35 : 55}
+                  height={isMobile ? 35 : 55}
+                />
+              </div>
+              <Link
+                href={"/"}
+                className={
+                  scrollRef.current < scrollY
+                    ? "scroll-translate-up"
+                    : "scroll-translate-down"
+                }
+              >
+                <span className="text-[12px] xs:text-[16px] md:text-[24px] leading-7 font-[800]">
+                  SOFTOZET
+                </span>
+              </Link>
+            </nav>
             <div
-              className=""
-              style={{
-                transform: `rotate(-${scrollY}deg)`,
-                transition: "transform 0.2s",
-              }}
-            >
-              <SvgLogo width={55} height={55} />
-            </div>
-            <Link
-              href={"/"}
-              className={
+              className={`${
                 scrollRef.current < scrollY
                   ? "scroll-translate-up"
                   : "scroll-translate-down"
-              }
+              } hidden md:flex w-[50%] justify-center rounded-full`}
             >
-              <span className="text-[12px] xs:text-[16px] md:text-[24px] leading-7 font-[800]">
-                SOFTOZET
-              </span>
-            </Link>
-          </nav>
-          <div
-            className={`${
-              scrollRef.current < scrollY
-                ? "scroll-translate-up"
-                : "scroll-translate-down"
-            } hidden md:flex w-[50%] h-[90%]  justify-center rounded-full`}
-          >
-            <div className="w-fit flex flex-row justify-between px-4 items-center gap-5 border-gradient-color ">
-              {navMenus.map((menu, index) => (
-                <nav
-                  key={index}
-                  className={`${
-                    menu.path === pathname
-                      ? "text-[#329aa1]"
-                      : " text-[#ffffff]"
-                  } text-[12px] font-[600]`}
-                >
-                  <Link href={menu.path}>{menu.display}</Link>
-                </nav>
-              ))}
+              <div className="w-fit flex flex-row justify-between px-4 py-3 items-center gap-5 border-gradient-color ">
+                {navMenus.map((menu, index) => (
+                  <nav
+                    key={index}
+                    className={`${
+                      menu.path === pathname
+                        ? "text-[#9932cc]"
+                        : " text-[#ffffff]"
+                    } text-[12px] font-[600]`}
+                  >
+                    <Link href={menu.path}>{menu.display}</Link>
+                  </nav>
+                ))}
+              </div>
             </div>
-          </div>
 
-          <nav className=" hidden md:flex text-[12px]  leading-7 font-[600] border-[3px] border-solid botton-gradient">
-            <Button
-              onClick={() => {}}
-              title="FREE CONSULTATION"
-              className=" px-2 py-[1px] cursor-pointer select-none"
-            />
-          </nav>
-          <span className="block md:hidden" onClick={hadleShowSidebar}>
-            <BiMenu className="w-6 h-6 cursor-pointer" />
-          </span>
+            <nav className=" hidden md:flex text-[12px]  leading-7 font-[600] border-[3px] border-solid botton-gradient">
+              <Button
+                onClick={() => router.push("/contact")}
+                title="GET IN TOUCH"
+                className=" px-2 cursor-pointer select-none"
+              />
+            </nav>
+            <span className="block md:hidden" onClick={hadleShowSidebar}>
+              <BiMenu className="w-6 h-6 cursor-pointer" />
+            </span>
+          </div>
         </div>
       </header>
     </>
